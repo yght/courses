67274d6f048c3502d9fdd70bfa4648706291dc50
arianght <arian.ght.82@gmail.com>
2020-03-30 00:59:09 +0430

    Update Program.cs

--- 1/first/first/Program.cs
+++ 1/first/first/Program.cs
@@ -10,9 +10,8 @@ namespace first
     {
         static void Main(string[] args)
         {
-
-            // You missed ; please put it and test again.
-            Console.WriteLine("Hello Arian ");
+            
+            Console.WriteLine("Hello worlllld");
         }
     }
 }
