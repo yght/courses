d2c00dea889d3422b9c1b0a0225a2077cffde48e
arianght <arian.ght.82@gmail.com>
2020-03-30 00:45:58 +0430

    test

--- 1/first/first/Program.cs
+++ 1/first/first/Program.cs
@@ -10,7 +10,7 @@ namespace first
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Arian ");
+            Console.WriteLine("Hello Arian ")
         }
     }
 }
