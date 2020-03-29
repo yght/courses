3b1967850a8d11e62dd8e3aab74efdd30509b8c3
Yousof <y.ghahremani@gmail.com>
2020-03-29 16:11:09 -0400

    changed the value

--- 1/first/first/Program.cs
+++ 1/first/first/Program.cs
@@ -10,7 +10,7 @@ namespace first
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("First ");
+            Console.WriteLine("Hello Arian ");
         }
     }
 }
