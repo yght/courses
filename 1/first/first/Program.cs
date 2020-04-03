6e00b81eb07e61161a79cbbed7a7bd107c9b3be4
Yousof <y.ghahremani@gmail.com>
2020-04-03 17:47:14 -0400

    Fixed
    English Message
    Condition

--- 1/first/first/Program.cs
+++ 1/first/first/Program.cs
@@ -11,14 +11,17 @@ namespace first
         static void Main(string[] args)
         {
             int age;
+            // ENGLISH: HOW OLD ARE YOU?
             Console.WriteLine("how much do you have?");
             age = Convert.ToInt32(Console.ReadLine());
             if (age >= 10)
             {
+                // >10 you are KID!??!?!? if (age <10) you are kid
                 Console.WriteLine("you are  kid");
             }
             else
             {
+                //Welocome to my program
                 Console.WriteLine("ok we;com to my programm");
 
             }
