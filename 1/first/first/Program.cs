c6a884b85e55dc675f365b5b4fc2062f9ede69bc
Yousof <y.ghahremani@gmail.com>
2020-04-03 15:12:07 -0400

    Update Program.cs
    Hello

--- 1/first/first/Program.cs
+++ 1/first/first/Program.cs
@@ -10,7 +10,7 @@ namespace first
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hellllllo0");
+            Console.WriteLine("HELLO");
         }
 
     }
