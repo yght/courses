e0b33d4205990002ba701b657f3a7f46503e2379
arianght <arian.ght.82@gmail.com>
2020-04-06 21:21:33 +0430

    copy
    coping from0 until 10

--- 1/first/first/Program.cs
+++ 1/first/first/Program.cs
@@ -11,45 +11,9 @@ namespace calculator
 
         static void Main(string[] args)
         {
-            while (true)
+           for(int i=0; i <=10;i++)
             {
-                int number1;
-                int number2;
-
-                
-                Console.WriteLine("enter a number");
-                number1 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("secound number?");
-                number2 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("ok. which operation do you want to do?");
-                string input = (Console.ReadLine());
-                
-                        
-                    
-                        if (input == "+")
-                        {
-                            Console.WriteLine(number1 + number2);
-                        }
-                       else if (input == "-")
-                        {
-                            Console.WriteLine(number1 - number2);
-                        }
-                      else  if (input == "/")
-                        {
-                            Console.WriteLine(number1 / number2);
-                        }
-
-                      else  if (input == "*")
-                        {
-                            Console.WriteLine(number1 * number2);
-                        }
-                   else
-                {
-                    Console.WriteLine("you have to enter (+ - / *)");
-                }
-
-                   
-                
+                Console.WriteLine(i);
             }
         }
 
