ca0dac94f3bbee7adcb004ac3b6f4675ba8e9f24
arianght <arian.ght.82@gmail.com>
2020-04-06 17:47:21 +0430

    calculator 3
    it is better

--- 1/first/first/Program.cs
+++ 1/first/first/Program.cs
@@ -4,51 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace first
+namespace calculator
 {
     class Program
     {
-        
+
         static void Main(string[] args)
         {
-           
-            int number1;
-            int number2;
-            int apply;
-            Console.WriteLine("it is a Calculator");
-            Console.WriteLine("enter a number");
-            number1=Convert.ToInt32 (Console.ReadLine());
-            Console.WriteLine("secound number?");
-           number2= Convert.ToInt32( Console.ReadLine());
-            Console.WriteLine("if you enter 1: sup up , if enter2: Subtraction ,if enter3: split , if enter4: beat");
-
-           apply = Convert.ToInt32( Console.ReadLine());
-            
-            if (apply == 1)
+            while (true)
             {
-               Console.WriteLine( number1 + number2);
-            }
-            if (apply == 2)
-            {
-                Console.WriteLine(number1 - number2);
-            }
-            if (apply == 3)
-            {
-                Console.WriteLine(number1 / number2);
-            }
-            if (apply == 4)
-            {
-                Console.WriteLine(number1 * number2);
-            }
-            else
-            {
-                Console.WriteLine("you have to enter 1 until 4");
-            }
+                int number1;
+                int number2;
 
+                
+                Console.WriteLine("enter a number");
+                number1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("secound number?");
+                number2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("ok. which operation do you want to do?");
+                string input = (Console.ReadLine());
+                
+                        
+                    
+                        if (input == "+")
+                        {
+                            Console.WriteLine(number1 + number2);
+                        }
+                       else if (input == "-")
+                        {
+                            Console.WriteLine(number1 - number2);
+                        }
+                      else  if (input == "/")
+                        {
+                            Console.WriteLine(number1 / number2);
+                        }
 
+                      else  if (input == "*")
+                        {
+                            Console.WriteLine(number1 * number2);
+                        }
+                   else
+                {
+                    Console.WriteLine("you have to enter (+ - / *)");
+                }
 
-
+                   
+                
+            }
         }
 
+
     }
+
 }
