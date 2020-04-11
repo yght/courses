9b44897fc4846d4b8a7fd3543c6b6a40d5ea6c02
arianght <arian.ght.82@gmail.com>
2020-04-11 22:37:46 +0430

    sentence

--- 1/first/first/Program.cs
+++ 1/first/first/Program.cs
@@ -11,41 +11,13 @@ namespace calculator
 
         static void Main(string[] args)
         {
-            int numbers;
-            Console.WriteLine("welcome which food do you like to eat? ");
-            Console.WriteLine("meat = $10");
-            Console.WriteLine("rice = $12");
-            Console.WriteLine("fish = $15");
-            string input=Console.ReadLine();
-            Console.WriteLine("how many  " + input + " do you want?");
-             numbers =Convert.ToInt32( Console.ReadLine());
 
-            if (input == "meat")
-            {
-                int tax = 10 * numbers * 8 / 100;
-                Console.WriteLine(tax);
-                Console.WriteLine("total = " + tax + 10 * numbers);
-            }
-
-            if (input == "fish")
-            {
-                int tax = 15 * numbers * 8 / 100;
-                Console.WriteLine(tax);
-                Console.WriteLine("total = " + tax + 15 * numbers);
-            }
-
-            if (input == "rice")
-            {
-                int tax = 12 * numbers * 8 / 100;
-                Console.WriteLine(tax);
-                Console.WriteLine("total = " + tax + 12 * numbers);
-            }
-
-
-
-
-
-
+            string sentence;
+            Console.Write("Enter String  : ");
+            sentence = Console.ReadLine();
+          
+            Console.WriteLine("Count of words :" + words.Length);
+            Console.ReadKey();
         }
 
 
