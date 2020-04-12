13946bc252dc84c5f11b3cac0e65734c2c7ef3b3
arianght <arian.ght.82@gmail.com>
2020-04-13 02:44:01 +0430

    accounting

--- 1/first/first/Program.cs
+++ 1/first/first/Program.cs
@@ -8,19 +8,62 @@ namespace calculator
 {
     class Program
     {
-
+       
         static void Main(string[] args)
         {
+            
+           
+                 char yes;
+                char no;
 
-            string sentence;
-            Console.Write("Enter String  : ");
-            sentence = Console.ReadLine();
+
+             Console.WriteLine("acciunting");
+             Console.WriteLine("how much is your balance? ");
+             int balance = Convert.ToInt32(Console.ReadLine());
+             if (balance.GetType() == typeof(int))
+             {
+                Console.WriteLine("The information was successfully recorded");
+
+             }
+             else
+             {
+                 Console.WriteLine("you have to enter number");
+             }
+             Console.WriteLine("Did you have any operation today?    1= yes   2 = no ");
+             string ask = Console.ReadLine();
+             if (ask == "1")
+            {
+                Console.WriteLine("enter your number ");
+                int number = Convert.ToInt32(Console.ReadLine());
+               
+
+                Console.WriteLine("what is your operation?  + - ");
+                string operation = Convert.ToString(Console.ReadLine());
+                 if (operation=="+")
+                {
+                    Console.WriteLine("New Inventory =" + balance + number);
+                }
+                 if(operation=="-")
+                {
+                    Console.WriteLine("New Inventory = " + (balance - number));
+
+                }
+              
+            }
           
-            Console.WriteLine("Count of words :" + words.Length);
-            Console.ReadKey();
+           
+
         }
+
 
 
     }
 
-}
+
+      
+    }
+
+
+
+
+
