f47ed1ed66a130994e91a22dd35260acdff2235a
arianght <arian.ght.82@gmail.com>
2020-04-13 22:13:55 +0430

    foreach

--- 1/first/first/Program.cs
+++ 1/first/first/Program.cs
@@ -11,10 +11,14 @@ namespace calculator
        
         static void Main(string[] args)
         {
-            var datetime = new DateTime(2020, 04, 13);
-            var hour = DateTime.Now;
-            var today = DateTime.Today;
-            Console.WriteLine("today is : " + hour + ":" + today);
+            var ten = new List<int> { 0, 2, 3, 4, 5, 6, 7, 8, 9,10 };
+            int count = 0;
+            foreach (int element in ten)
+            {
+                count++;
+                Console.WriteLine($"number #{count}: {element}");
+            }
+            Console.WriteLine($"Number of numbers: {count}");
 
         }
 
